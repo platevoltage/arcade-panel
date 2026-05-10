30806eabd578c0954488f5642d1d7cc8d4b2e86b
Garrett Corbin <jgarrettcorbin@gmail.com>
2026-05-09 23:12:19 -0700

    changed pins

--- src/pins.h
+++ src/pins.h
@@ -1,31 +1,29 @@
-#define BUTTON_PIN_0_P1 6  // B // bottom row left to right
-#define BUTTON_PIN_1_P1 4  // A
-#define BUTTON_PIN_2_P1 2  // L1
-#define BUTTON_PIN_3_P1 1  // R1
-#define BUTTON_PIN_4_P1 7  // Y // top row left to right
-#define BUTTON_PIN_5_P1 5  // X
-#define BUTTON_PIN_6_P1 3  // L2
-#define BUTTON_PIN_7_P1 12 // L3
-#define BUTTON_PIN_8_P1 9  // select
-#define BUTTON_PIN_9_P1 8  // start
+#define BUTTON_PIN_0_P1 14 // B // bottom row left to right
+#define BUTTON_PIN_1_P1 12 // A
+#define BUTTON_PIN_2_P1 10 // L1
+#define BUTTON_PIN_3_P1 46 // R1
+#define BUTTON_PIN_4_P1 13 // Y // top row left to right
+#define BUTTON_PIN_5_P1 11 // X
+#define BUTTON_PIN_6_P1 9  // L2
+#define BUTTON_PIN_7_P1 3  // L3
+#define BUTTON_PIN_8_P1 20 // select
+#define BUTTON_PIN_9_P1 19 // start
 
-#define BUTTON_PIN_0_P2 17 // bottom row left to right
-#define BUTTON_PIN_1_P2 21
-#define BUTTON_PIN_2_P2 37
-#define BUTTON_PIN_3_P2 39
-#define BUTTON_PIN_4_P2 34 // top row left to right
-#define BUTTON_PIN_5_P2 36
-#define BUTTON_PIN_6_P2 38
-#define BUTTON_PIN_7_P2 40
-#define BUTTON_PIN_8_P2 11 // select
-#define BUTTON_PIN_9_P2 10 // start
+#define BUTTON_PIN_0_P2 8 // bottom row left to right
+#define BUTTON_PIN_1_P2 17
+#define BUTTON_PIN_2_P2 15
+#define BUTTON_PIN_3_P2 6
+#define BUTTON_PIN_4_P2 18 // top row left to right
+#define BUTTON_PIN_5_P2 16
+#define BUTTON_PIN_6_P2 7
+#define BUTTON_PIN_7_P2 5
+#define BUTTON_PIN_8_P2 40 // select
+#define BUTTON_PIN_9_P2 41 // start
 
-#define BUTTON_LIGHTS_DATA_PIN 16
-#define BUTTON_LIGHTS_CLOCK_PIN 18
-#define BUTTON_LIGHTS_LATCH_PIN 35
+#define BUTTON_LIGHTS_DATA_PIN 1
+#define BUTTON_LIGHTS_CLOCK_PIN 2
+#define BUTTON_LIGHTS_LATCH_PIN 42
 
-#define RING_DATA_PIN 13
+#define RING_DATA_PIN 39
 
-// #define AVAILABLE_PIN_1 13
-#define AVAILABLE_PIN_2 14
-#define AVAILABLE_PIN_3 15 // no pigtail
+#define ONBOARD_RGB_DATA_PIN 38
