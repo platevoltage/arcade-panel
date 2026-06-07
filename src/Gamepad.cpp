2c6425af79c08b6667b86163c421c55182786559
Garrett Corbin <jgarrettcorbin@gmail.com>
2026-06-07 10:09:17 -0700

    storage bs

--- src/Gamepad.cpp
+++ src/Gamepad.cpp
@@ -35,198 +35,198 @@ void Gamepad::task() {
   if (!player1.ready() || !player2.ready())
     return;
   // Serial.println("Gamepad task");
-  Serial.println("No pressing buttons");
-  gp.x = 0;
-  gp.y = 0;
-  gp.z = 0;
-  gp.rz = 0;
-  gp.rx = 0;
-  gp.ry = 0;
-  gp.hat = 0;
-  gp.buttons = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // Serial.println("No pressing buttons");
+  // gp.x = 0;
+  // gp.y = 0;
+  // gp.z = 0;
+  // gp.rz = 0;
+  // gp.rx = 0;
+  // gp.ry = 0;
+  // gp.hat = 0;
+  // gp.buttons = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD UP
-  Serial.println("Hat/DPAD UP");
-  gp.hat = 1; // GAMEPAD_HAT_UP;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD UP
+  // Serial.println("Hat/DPAD UP");
+  // gp.hat = 1; // GAMEPAD_HAT_UP;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD UP RIGHT
-  Serial.println("Hat/DPAD UP RIGHT");
-  gp.hat = 2; // GAMEPAD_HAT_UP_RIGHT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD UP RIGHT
+  // Serial.println("Hat/DPAD UP RIGHT");
+  // gp.hat = 2; // GAMEPAD_HAT_UP_RIGHT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD RIGHT
-  Serial.println("Hat/DPAD RIGHT");
-  gp.hat = 3; // GAMEPAD_HAT_RIGHT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD RIGHT
+  // Serial.println("Hat/DPAD RIGHT");
+  // gp.hat = 3; // GAMEPAD_HAT_RIGHT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD DOWN RIGHT
-  Serial.println("Hat/DPAD DOWN RIGHT");
-  gp.hat = 4; // GAMEPAD_HAT_DOWN_RIGHT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD DOWN RIGHT
+  // Serial.println("Hat/DPAD DOWN RIGHT");
+  // gp.hat = 4; // GAMEPAD_HAT_DOWN_RIGHT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD DOWN
-  Serial.println("Hat/DPAD DOWN");
-  gp.hat = 5; // GAMEPAD_HAT_DOWN;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD DOWN
+  // Serial.println("Hat/DPAD DOWN");
+  // gp.hat = 5; // GAMEPAD_HAT_DOWN;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD DOWN LEFT
-  Serial.println("Hat/DPAD DOWN LEFT");
-  gp.hat = 6; // GAMEPAD_HAT_DOWN_LEFT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD DOWN LEFT
+  // Serial.println("Hat/DPAD DOWN LEFT");
+  // gp.hat = 6; // GAMEPAD_HAT_DOWN_LEFT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD LEFT
-  Serial.println("Hat/DPAD LEFT");
-  gp.hat = 7; // GAMEPAD_HAT_LEFT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD LEFT
+  // Serial.println("Hat/DPAD LEFT");
+  // gp.hat = 7; // GAMEPAD_HAT_LEFT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD UP LEFT
-  Serial.println("Hat/DPAD UP LEFT");
-  gp.hat = 8; // GAMEPAD_HAT_UP_LEFT;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD UP LEFT
+  // Serial.println("Hat/DPAD UP LEFT");
+  // gp.hat = 8; // GAMEPAD_HAT_UP_LEFT;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Hat/DPAD CENTER
-  Serial.println("Hat/DPAD CENTER");
-  gp.hat = 0; // GAMEPAD_HAT_CENTERED;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Hat/DPAD CENTER
+  // Serial.println("Hat/DPAD CENTER");
+  // gp.hat = 0; // GAMEPAD_HAT_CENTERED;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 1 UP
-  Serial.println("Joystick 1 UP");
-  gp.x = 0;
-  gp.y = -127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 1 UP
+  // Serial.println("Joystick 1 UP");
+  // gp.x = 0;
+  // gp.y = -127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 1 DOWN
-  Serial.println("Joystick 1 DOWN");
-  gp.x = 0;
-  gp.y = 127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 1 DOWN
+  // Serial.println("Joystick 1 DOWN");
+  // gp.x = 0;
+  // gp.y = 127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 1 RIGHT
-  Serial.println("Joystick 1 RIGHT");
-  gp.x = 127;
-  gp.y = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 1 RIGHT
+  // Serial.println("Joystick 1 RIGHT");
+  // gp.x = 127;
+  // gp.y = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 1 LEFT
-  Serial.println("Joystick 1 LEFT");
-  gp.x = -127;
-  gp.y = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 1 LEFT
+  // Serial.println("Joystick 1 LEFT");
+  // gp.x = -127;
+  // gp.y = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 1 CENTER
-  Serial.println("Joystick 1 CENTER");
-  gp.x = 0;
-  gp.y = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 1 CENTER
+  // Serial.println("Joystick 1 CENTER");
+  // gp.x = 0;
+  // gp.y = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 2 UP
-  Serial.println("Joystick 2 UP");
-  gp.z = 0;
-  gp.rz = 127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 2 UP
+  // Serial.println("Joystick 2 UP");
+  // gp.z = 0;
+  // gp.rz = 127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 2 DOWN
-  Serial.println("Joystick 2 DOWN");
-  gp.z = 0;
-  gp.rz = -127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 2 DOWN
+  // Serial.println("Joystick 2 DOWN");
+  // gp.z = 0;
+  // gp.rz = -127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 2 RIGHT
-  Serial.println("Joystick 2 RIGHT");
-  gp.z = 127;
-  gp.rz = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 2 RIGHT
+  // Serial.println("Joystick 2 RIGHT");
+  // gp.z = 127;
+  // gp.rz = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 2 LEFT
-  Serial.println("Joystick 2 LEFT");
-  gp.z = -127;
-  gp.rz = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 2 LEFT
+  // Serial.println("Joystick 2 LEFT");
+  // gp.z = -127;
+  // gp.rz = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Joystick 2 CENTER
-  Serial.println("Joystick 2 CENTER");
-  gp.z = 0;
-  gp.rz = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Joystick 2 CENTER
+  // Serial.println("Joystick 2 CENTER");
+  // gp.z = 0;
+  // gp.rz = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 1 UP
-  Serial.println("Analog Trigger 1 UP");
-  gp.rx = 127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 1 UP
+  // Serial.println("Analog Trigger 1 UP");
+  // gp.rx = 127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 1 DOWN
-  Serial.println("Analog Trigger 1 DOWN");
-  gp.rx = -127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 1 DOWN
+  // Serial.println("Analog Trigger 1 DOWN");
+  // gp.rx = -127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 1 CENTER
-  Serial.println("Analog Trigger 1 CENTER");
-  gp.rx = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 1 CENTER
+  // Serial.println("Analog Trigger 1 CENTER");
+  // gp.rx = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 2 UP
-  Serial.println("Analog Trigger 2 UP");
-  gp.ry = 127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 2 UP
+  // Serial.println("Analog Trigger 2 UP");
+  // gp.ry = 127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 2 DOWN
-  Serial.println("Analog Trigger 2 DOWN");
-  gp.ry = -127;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 2 DOWN
+  // Serial.println("Analog Trigger 2 DOWN");
+  // gp.ry = -127;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Analog Trigger 2 CENTER
-  Serial.println("Analog Trigger 2 CENTER");
-  gp.ry = 0;
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Analog Trigger 2 CENTER
+  // Serial.println("Analog Trigger 2 CENTER");
+  // gp.ry = 0;
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 
-  // Test buttons (up to 32 buttons)
-  for (int i = 0; i < 32; ++i) {
-    Serial.print("Pressing button ");
-    Serial.println(i);
-    gp.buttons = (1U << i);
-    player1.sendReport(0, &gp, sizeof(gp));
-    player2.sendReport(0, &gp, sizeof(gp));
-    delay(10);
-  }
+  // // Test buttons (up to 32 buttons)
+  // for (int i = 0; i < 32; ++i) {
+  //   Serial.print("Pressing button ");
+  //   Serial.println(i);
+  //   gp.buttons = (1U << i);
+  //   player1.sendReport(0, &gp, sizeof(gp));
+  //   player2.sendReport(0, &gp, sizeof(gp));
+  //   delay(10);
+  // }
 
-  // Random touch
-  Serial.println("Random touch");
-  gp.x = random(-127, 128);
-  gp.y = random(-127, 128);
-  gp.z = random(-127, 128);
-  gp.rz = random(-127, 128);
-  gp.rx = random(-127, 128);
-  gp.ry = random(-127, 128);
-  gp.hat = random(0, 9);
-  gp.buttons = random(0, 0xffff);
-  player1.sendReport(0, &gp, sizeof(gp));
-  player2.sendReport(0, &gp, sizeof(gp));
+  // // Random touch
+  // Serial.println("Random touch");
+  // gp.x = random(-127, 128);
+  // gp.y = random(-127, 128);
+  // gp.z = random(-127, 128);
+  // gp.rz = random(-127, 128);
+  // gp.rx = random(-127, 128);
+  // gp.ry = random(-127, 128);
+  // gp.hat = random(0, 9);
+  // gp.buttons = random(0, 0xffff);
+  // player1.sendReport(0, &gp, sizeof(gp));
+  // player2.sendReport(0, &gp, sizeof(gp));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "KeyboardEmu.h"
 #include "Storage.h"
 #include <Arduino.h>
+// bool core1_disable_systick = true;
+// bool core1_separate_stack = true;
 
 void setup() {
   Serial.begin(115200);
@@ -13,4 +15,12 @@ void setup() {
 void loop() {
   gamepad.task();
   keyboard.task();
+  // Serial.println("loop 0");
+  // delay(500);
+}
+void setup1() { delay(5000); }
+void loop1() {
+  // Serial.println(Serial.available());
+  // Serial.println("loop 1");
+  delay(1500);
 }
